c3dd947580120af6b6d273e5e4efc831707470be
Lampedrazago29 <la.pedrazagonzalez@student.unisi.it>
2018-05-29 15:43:42 +0200

    LMPG-29-05-2018 | Tests for Rh and ASR

---
+++ TEST/1xio_ARM_input/wt_1xio_ARM_input/1xio.ARM_out/1xio.ARM_VMD.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+vmd 1xio.ARM_out.pdb -e 1xio.tcl
---
+++ TEST/1xio_ARM_input/wt_1xio_ARM_input/infoIon1xio.ARM.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+ 
+/Users/laurapedraza/Documents/Software/put_ions_good/./ion.x << EOF 
+1xio.ARM.pqr
+6
+9
+27
+30
+96
+97
+151
+153
+155
+167
+222
+-3
+7
+4
+57
+75
+120
+123
+125
+198
+EOF
